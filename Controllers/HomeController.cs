5196995b9d8a380e26fcfdacd219732280e39f13
joedono <fjzusi@gmail.com>
2020-08-17 11:16:42 -0400

    Removed initial project files and added HomeController

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿namespace KM_Technical_Assessment.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Caching.Memory;
+
+    [ApiController]
+    public class HomeController : Controller
+    {
+        #region Fields
+
+        private IMemoryCache memoryCache;
+
+        #endregion
+
+        #region Constructor
+
+        public HomeController(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        #endregion
+
+        #region Actions
+
+        [HttpGet]
+        [Route("initialize")]
+        public string Initialize()
+        {
+            return "Initialized";
+        }
+
+        #endregion
+
+        #region Helpers
+
+        #endregion
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
 namespace KM_Technical_Assessment
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -25,6 +18,7 @@ namespace KM_Technical_Assessment
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
             services.AddControllers();
         }
 
@@ -36,11 +30,7 @@ namespace KM_Technical_Assessment
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
