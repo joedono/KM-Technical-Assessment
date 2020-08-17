f9885d3bb69bd60c6b6442c2f49d484a65b84492
joedono <fjzusi@gmail.com>
2020-08-17 11:22:03 -0400

    Added request method stubs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿namespace KM_Technical_Assessment.Controllers
 {
+    using KM_Technical_Assessment.Models;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Caching.Memory;
+    using System;
 
     [ApiController]
     public class HomeController : Controller
@@ -25,9 +27,41 @@
 
         [HttpGet]
         [Route("initialize")]
-        public string Initialize()
+        public KMResponse Initialize()
         {
-            return "Initialized";
+            // TODO Initialize Game
+
+            return new KMResponse
+            {
+                msg = "Initialize",
+                body = new KMResponseBody
+                {
+                    heading = "Player 1",
+                    message = "Awaiting Player 1's Move"
+                }
+            };
+        }
+
+        [HttpPost]
+        [Route("node-clicked")]
+        public KMResponse NodeClicked([FromBody] KMPoint point)
+        {
+            return new KMResponse
+            {
+                msg = "Initialize",
+                body = new KMResponseBody
+                {
+                    heading = "Player 1",
+                    message = "Awaiting Player 1's Move"
+                }
+            };
+        }
+
+        [HttpPost]
+        [Route("error")]
+        public void Error([FromBody] KMError error)
+        {
+            Console.WriteLine(error.error);
         }
 
         #endregion
