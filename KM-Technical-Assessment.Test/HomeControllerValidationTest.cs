7b327ea8251525e3439eeac5861975ac73128c21
joedono <fjzusi@gmail.com>
2020-08-18 19:48:19 -0400

    Added unit test project

---
+++ KM-Technical-Assessment.Test/HomeControllerValidationTest.cs
@@ -0,0 +1,13 @@
+﻿namespace KM_Technical_Assessment.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class HomeControllerValidationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
